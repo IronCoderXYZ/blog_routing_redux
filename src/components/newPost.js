8ad3555862df86fcb4219bae10d520d7e7f1ea1f
Mitch Hankins <mitchhankins92@gmail.com>
2018-05-10 22:31:08 -0600

    Adding form components to newPost

--- src/components/newPost.js
+++ src/components/newPost.js
@@ -1,9 +1,27 @@
 import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
+  renderField(field) {
+    return (
+      <div className="form-group">
+        <label>{field.label}</label>
+        <input {...field.input} className="form-control" />
+      </div>
+    );
+  }
+
   render() {
-    return <div>New Post</div>;
+    return (
+      <form>
+        <Field name="title" label="Title" component={this.renderField} />
+        <Field name="content" label="Content" component={this.renderField} />
+        <Field name="tags" label="Tags" component={this.renderField} />
+      </form>
+    );
   }
 }
 
-export default PostsNew;
+export default reduxForm({
+  form: 'NewPostForm'
+})(PostsNew);
