cad6b240d2607c11b21a89e21e4c9d683959ef96
Mitch Hankins <mitchhankins92@gmail.com>
2018-05-10 22:39:43 -0600

    Adding validate function reduxForm newPost

--- src/components/newPost.js
+++ src/components/newPost.js
@@ -13,15 +13,36 @@ class PostsNew extends Component {
 
   render() {
     return (
-      <form>
+      <form className="col-sm-6 mx-auto">
         <Field name="title" label="Title" component={this.renderField} />
+        <Field
+          name="categories"
+          label="Categories"
+          component={this.renderField}
+        />
         <Field name="content" label="Content" component={this.renderField} />
-        <Field name="tags" label="Tags" component={this.renderField} />
       </form>
     );
   }
 }
 
+function validate(values) {
+  const errors = {};
+  switch (values) {
+    case !values.title:
+      errors.title = 'Please provide a title';
+    case !values.categories:
+      errors.title = 'Please provide the categories';
+    case !values.content:
+      errors.title = 'Please provide your content';
+  }
+  // if (!values.title) errors.title = 'Please provide a title';
+  // if (!values.categories) errors.categories = 'Please provide the categories';
+  // if (!values.content) errors.content = 'Please provide your content';
+  return errors;
+}
+
 export default reduxForm({
+  validate,
   form: 'NewPostForm'
 })(PostsNew);
